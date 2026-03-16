ebeadfb12e1f9408c568283d012c6a7155774077
d_machilsky <d.machilsky@pay-lab.ru>
2026-03-16 15:56:17 +0300

    SCRUM-35 Auth view model

--- frontend/AdventureMaker/app/src/main/java/com/softcat/adventuremaker/AdventureMakerApplication.kt
+++ frontend/AdventureMaker/app/src/main/java/com/softcat/adventuremaker/AdventureMakerApplication.kt
@@ -2,11 +2,18 @@ package com.softcat.adventuremaker
 
 import android.app.Application
 import com.google.firebase.FirebaseApp
+import com.softcat.adventuremaker.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.GlobalContext.startKoin
 
 class AdventureMakerApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
         FirebaseApp.initializeApp(this)
+        startKoin {
+            androidContext(this@AdventureMakerApplication)
+            modules(viewModelModule)
+        }
     }
 }
