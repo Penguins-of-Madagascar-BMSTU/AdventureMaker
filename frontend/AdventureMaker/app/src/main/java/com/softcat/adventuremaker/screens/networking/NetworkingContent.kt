5c583777f6c1bb9713d988c5e1a23185028d958f
d_machilsky <d.machilsky@pay-lab.ru>
2026-03-20 12:46:21 +0300

    SCRUM-40 favourite screen view model is added and favourite screen design is implemented.

--- frontend/AdventureMaker/app/src/main/java/com/softcat/adventuremaker/screens/networking/NetworkingContent.kt
+++
@@ -1,8 +0,0 @@
-package com.softcat.adventuremaker.screens.networking
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun NetworkingContent() {
-
-}
--- frontend/AdventureMaker/app/src/main/java/com/softcat/adventuremaker/screens/utils/UtilsContent.kt
+++
@@ -1,8 +0,0 @@
-package com.softcat.adventuremaker.screens.utils
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun UtilsContent() {
-
-}
