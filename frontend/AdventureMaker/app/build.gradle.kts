37875c6b2c22d0e2f05d5595db93fbe2ccdbb41e
Olesya <r.tevo@yandex.ru>
2026-03-23 11:03:24 +0300

    SCRUM-33 check library announcements

--- frontend/AdventureMaker/app/build.gradle.kts
+++ frontend/AdventureMaker/app/build.gradle.kts
@@ -63,15 +63,13 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.runtime:runtime-livedata")
-    implementation("androidx.compose.material:material-icons-extended")
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.runtime.livedata)
+    implementation(libs.androidx.compose.material.icons.extended)
 
-    implementation(platform("androidx.compose:compose-bom:2024.06.00"))
-
-    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation(libs.androidx.compose.ui.tooling)
 
     implementation(project(":domain"))
     implementation(project(":data"))
