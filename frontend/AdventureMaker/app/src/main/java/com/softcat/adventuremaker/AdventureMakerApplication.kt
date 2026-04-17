83ab8e9633a95e7b9b400fa5580eb94dbe3a3b4f
Rockio <berlin.operators@gmail.com>
2026-04-17 14:42:47 +0300

    AI TEST

--- frontend/AdventureMaker/app/src/main/java/com/softcat/adventuremaker/AdventureMakerApplication.kt
+++ frontend/AdventureMaker/app/src/main/java/com/softcat/adventuremaker/AdventureMakerApplication.kt
@@ -10,7 +10,7 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.GlobalContext.startKoin
 
 class AdventureMakerApplication: Application() {
-
+// HERE IS AI TEST
     val dataStore by preferencesDataStore(
         name = DATASTORE_NAME
     )
